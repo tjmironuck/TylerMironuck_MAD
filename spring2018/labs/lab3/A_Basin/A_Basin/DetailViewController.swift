21302569215851ac7dfb123abc0493fe5171c6ea
tjmironuck <31741527+tjmironuck@users.noreply.github.com>
2018-02-27 19:48:10 -0700

    Lab3 with image info added

--- spring2018/labs/lab3/A_Basin/A_Basin/DetailViewController.swift
+++ spring2018/labs/lab3/A_Basin/A_Basin/DetailViewController.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageInfoLabel: UITextView!
     var imageName : String?
     
     override func viewWillAppear(_ animated: Bool) {
         if let name = imageName {
             imageView.image = UIImage(named: name)
+            switch name {
+            case "abasin1":
+                imageInfoLabel.text = "Arapahoe Basin was founded in December of 1946 by five pioneers of skiing: Larry Jump, Thor Groswold, Sandy Schauffer, Dick Durrance and Max Dercum. You can find runs named after them on \"Founders' Ridge\" in west Montezuma Bowl."
+            case "abasin2":
+                imageInfoLabel.text = "Arapahoe Basin has 468 total acres, 339 lift-served acres in The Beavers, and 129 hike-back acres in The Steep Gullies. The hike back trail will lead from the bottom of The Beaves & Steep Gullies terrain to the bottom terminal of the Pallavicini Lift. Hike time is approximately 15-30 minuts, depending on equipment and fitness level."
+            case "abasin3":
+                imageInfoLabel.text = "Underway at Arapahoe Basin is a massive terrain expansion that will extend lift-serviced terrain ino the Beavers. The over 400-acre expansion will include intermediate terrain, glades, and will incorporate The Steep Gullies into its patrolled area."
+            case "abasin4":
+                imageInfoLabel.text = "In 1946, visitors paid $1.25 to access the mountain. 72 years later, an adult full day ticket costs $92 until April 30."
+            case "abasin5":
+                imageInfoLabel.text = "Arapahoe Basin is known for its advanced and expert ski terrain. The East Wall boasts a top elevation of 13,050 feet, one of the highest in-bounds skiable areas in North America."
+            case "abasin6":
+                imageInfoLabel.text = "Arapahoe Basin is nationally recognized for its efforts to reduce the resort's environmental footprint. The ski area has won National Ski Areas Association (NSAA) awards for waste reduction, visual impacts, and water conservation."
+            case "abasin7":
+                imageInfoLabel.text = "Celebrate spring on the slope with the annual Shakin' at the Basin summer concert series and Swimsuit Parade."
+            case "abasin8":
+                imageInfoLabel.text = "If you haven't used the two free ski days for your kids at A-Basin this season, now is the time to let them dress up in there goofiest costumes on the slopes. Kids ages 6 to 12 have two free ski days."
+            case "abasin9":
+                imageInfoLabel.text = "The tradition of skiing in swimsuits dates back to the 1970s."
+            case "abasin10":
+                imageInfoLabel.text = "Arapahoe Basin Swimsuit Parade, May 2016."
+            case "abasin11":
+                imageInfoLabel.text = "Arapahoe Basin's Early Riser parking lot is home to a stretch of prime real estate known as \"The Beach\". The Beach backs up to the three lower mountain chairlifts and is the perfect spot to kick back between runs with family and friends. It's a sceen unique to the Basin, and a guaranteed good time! The Beach often plays home to birthday celebrations, reunions, company parties, and other special events."
+            case "abasin12":
+                imageInfoLabel.text = "A-Basin i the abbreviation for Arapahoe Basin, sometimes also referred to as just \"The Basin.\"."
+
+            default:
+                imageInfoLabel.text = "Something went wrong"
+            }
         }
+        
     }
     
     
